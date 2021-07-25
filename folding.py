a0605c89da2854b7a8bcef153472f5ed7f1d5e61
Alex <dantrag.666@gmail.com>
2021-07-25 17:39:34 +0200

    Center and crop output images to minimum size

--- folding.py
+++ folding.py
@@ -19,6 +19,15 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __str__(self):
+        return str((self.x, self.y))
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
     def round(self):
         self.x = int(round(self.x))
         self.y = int(round(self.y))
@@ -102,27 +111,55 @@ class Bild:
                     reflected = axis.reflect(point)
                     reflected.round()
                     if 0 <= reflected.x < self.h and 0 <= reflected.y < self.w:
-                        if self.pixels[reflected.x, reflected.y] == 0:
-                            folded.append(reflected)
+                        folded.append(reflected)
                         value = self.pixels[point.x, point.y]    
                         self.pixels[reflected.x, reflected.y] += value
                         self.pixels[point.x, point.y] -= value
                     else:
                         print("Warning! Folded out of the frame")
-                for point in part1 + part2 + folded:
-                    if self.pixels[point.x, point.y] != 0:
+                for point in list(set(part1 + part2 + folded)):
+                    if self.pixels[point.x, point.y] > 0:
                         self.nonzero.append(point)
+
                 self.foldcount += 1
                 return True
             attempts -= 1
 
         return False
 
+    def center(self):
+        deltax = (self.h - self.x_min() - self.x_max() - 1) // 2
+        deltay = (self.w - self.y_min() - self.y_max() - 1) // 2
+
+        self.pixels = np.roll(self.pixels, deltax, axis=0)
+        self.pixels = np.roll(self.pixels, deltay, axis=1)
+
+        self.nonzero = [Point(p.x + deltax, p.y + deltay) for p in self.nonzero]
+
+    def x_min(self):
+        return min(self.nonzero, key=lambda p: p.x).x
+
+    def y_min(self):
+        return min(self.nonzero, key=lambda p: p.y).y
+
+    def x_max(self):
+        return max(self.nonzero, key=lambda p: p.x).x
+
+    def y_max(self):
+        return max(self.nonzero, key=lambda p: p.y).y
+
+
 seed = Bild(Image.open("input/unfolded.png")
                  .resize((100, 100), resample=Image.NEAREST)
                  .convert('L'))
 
+seed.center()
+h = seed.x_max() - seed.x_min() + 1
+w = seed.y_max() - seed.y_min() + 1
+
 data = [seed]
+
+np.set_printoptions(threshold=np.inf)
 
 for i in range(100):
     while True:
@@ -131,8 +168,18 @@ for i in range(100):
             break
     image = seed.copy()
     image.fold(0.2)
+    image.center()
     data.append(image)
-    saved = Image.fromarray(image.pixels, 'L')
+
+    h = max(h, image.x_max() - image.x_min() + 1)
+    w = max(w, image.y_max() - image.y_min() + 1)
+
+for i in range(len(data)):
+    image = data[i]
+    saved = Image.fromarray(image.pixels, 'L').crop(((image.w - w) // 2,
+                                                     (image.h - h) // 2,
+                                                     image.w - (image.w - w + 1) // 2,
+                                                     image.h - (image.h - h + 1) // 2))
     saved.save("output/image%d.png" % i)
 
 print(len(data))
