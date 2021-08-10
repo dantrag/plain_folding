8ed62b4834e264a8368a2fc036d9072f33555d9a
Alex <dantrag.666@gmail.com>
2021-08-10 14:06:05 +0200

    Added the perturbation code

--- folding.py
+++ folding.py
@@ -4,7 +4,7 @@ from math import pi, sin, cos
 from PIL import Image
 import numpy as np
 
-from geometry import Point, Line, extract_contours, split_into_components, move_points_connected, bfs_unless
+from geometry import *
 
 class Bild:
     def __init__(self, image):
@@ -38,6 +38,7 @@ class Bild:
         """
         attempts = 100
         while attempts:
+            attempts -= 1
             center = random.choice(self.nonzero)
             center = Point(center.x + 0.5, center.y + 0.5)
             xy_aligned = random.random() <= xy_axis_bias
@@ -58,6 +59,7 @@ class Bild:
                     part2.append(point)
             if len(part1) > len(part2):
                 part1, part2 = part2, part1
+
             if len(part1) / len(self.nonzero) >= threshold:
                 # fold each connected component separately
                 pieces = split_into_components(set(part1))
@@ -146,9 +148,214 @@ class Bild:
 
                 self.foldcount += 1
                 return True
-            attempts -= 1
 
         return False
+
+    def perturb(self, num_samples=10, resolution=20, integral_tolerance=1):
+        # resolution = width of a perturbation window
+        resolution = resolution // 2 * 2 + 1
+
+        # sample seed points for perturbations from a contour
+        contours = extract_contours(set(self.nonzero))
+        if not contours:
+            return
+
+        # contour should be one, just in case use the longest one
+        contour = contours[0]
+        seeds = []
+
+        for _ in range(num_samples):
+            # make sure all seed points are separated, so that
+            # perturbation windows do not overlap
+            tries = 50
+            seed = None
+            while tries:
+                tries -= 1
+                seed = random.choice(contour)
+                is_separated_well = True
+                for previous_seed in seeds:
+                    if seed.closer_than(previous_seed, resolution):
+                        is_separated_well = False
+                        break
+                if is_separated_well:
+                    break
+            if seed:
+                seeds.append(seed)
+
+        for seed in seeds:
+            # shrink the perturbation window until non-zero pixels inside have
+            # a single value only; split the window into square slices;
+            # perturbation window is a square with side (size * 2 - 1)
+            border = []
+            nonzero_value = 0
+            size = 0
+            for i in range(resolution // 2 + 1):
+                slice = [Point(x, seed.y - i) for x in range(seed.x - i,
+                                                             seed.x + i)] +\
+                        [Point(x, seed.y + i) for x in range(seed.x - i + 1,
+                                                             seed.x + i + 1)] +\
+                        [Point(seed.x - i, y) for y in range(seed.y - i + 1,
+                                                             seed.y + i + 1)] +\
+                        [Point(seed.x + i, y) for y in range(seed.y - i,
+                                                             seed.y + i)]
+                min_value = 256
+                max_value = 0
+                for point in slice:
+                    value = self.pixels[point.x, point.y]
+                    if value > 0:
+                        min_value = min(value, min_value)
+                        max_value = max(value, max_value)
+                        if min_value != max_value:
+                            break
+
+                if min_value != max_value and max_value != 0:
+                    break
+                else:
+                    if not nonzero_value and max_value != 0:
+                        nonzero_value = max_value
+                    if max_value != nonzero_value:
+                        break
+                    border = slice
+                    size += 1            
+            
+            if size < 3:
+                print("Too small window size")
+                continue
+            
+            # find the part of a contour that falls into the window
+            border = set(border)
+            original_contour = set()
+            original_filled = set()
+
+            for x in range(seed.x - size + 1, seed.x + size):
+                for y in range(seed.y - size + 1, seed.y + size):
+                    point = Point(x, y)
+                    if point in contour:
+                        original_contour.add(point)
+                    if self.pixels[x, y] > 0:
+                        original_filled.add(point)
+
+            # only allow windows in which a single piece of contour is captured
+            if len(split_into_components(original_contour)) > 1:
+                print("Multiple contours in the window")
+                continue
+
+            # find two ends of the contour inside the window lying on its border
+            graph = graph_from_points(original_contour)
+            depths = bfs_distances(graph, [seed])
+            start = None
+            for depth, point in enumerate(depths):
+                if point in border:
+                    if not start or depths[start] < depth:
+                        start = point
+            if not start:
+                print("Not found a start")
+                continue
+
+            depths = bfs_distances(graph, [start])
+            finish = None
+            for depth, point in enumerate(depths):
+                if point in border:
+                    if not finish or depths[finish] < depth:
+                        finish = point
+            if not finish or start == finish:
+                print("Not found a finish")
+                continue
+
+            # find a point on the window border that is inside the filled region
+
+            inside_points = []
+            for point in border:
+                if point in original_filled and not point in original_contour:
+                    inside_points.append(point)
+            if not inside_points:
+                print("No seed points for BFS")
+                continue
+
+            # now we make a new contour Bezier segment between start and finish
+            sampling_area = list(border)
+            sampling_area.remove(start)
+            sampling_area.remove(finish)
+            
+            # first try several random curves and evaluate filled integral
+
+            def apply_new_curve(curve: Bezier):
+                def in_bounds(point):
+                    return seed.x - size + 1 <= point.x < seed.x + size and \
+                           seed.y - size + 1 <= point.y < seed.y + size
+
+                curve_pixels = set()
+                for t in np.linspace(0, 1, (size * 2 + 1) * 5):
+                    curve_pixels.add(curve.evaluate(t))
+
+                # fill the correct area bounded by the curve and the window
+                starts = set(inside_points)
+                for point in curve_pixels:
+                    if point in starts:
+                        starts.remove(point)
+                if not starts:
+                    return []
+
+                #boundary = curve_pixels.union(border)
+                filled = bfs_unless(starts,
+                                    lambda point: point in curve_pixels or \
+                                                  not in_bounds(point))
+                return filled.union(curve_pixels)
+
+            samples = []
+            tries = 30
+            best_curve = None
+            best_integral = None
+            while tries:
+                tries -= 1
+                P1 = random.choice(sampling_area)
+                P2 = random.choice(sampling_area)
+                curve = Bezier(start, P1, P2, finish)
+                integral = len(apply_new_curve(curve))
+                if integral >= len(original_filled):
+                    if not best_integral or integral < best_integral:
+                        best_curve, best_integral = curve, integral
+            if not best_curve:
+                print("Not a single curve with big enough integral")
+                continue
+
+            # if the filled area is too large, shrink it by scaling the curve
+            # but first check if the smallest possible curve gives smaller area
+            
+            integral = len(original_filled)
+            min_curve = Bezier(start, start, finish, finish)
+            if len(apply_new_curve(min_curve)) > integral + integral_tolerance:
+                print("Even straight line gives too big integral")
+                continue
+
+            min_factor = 0
+            max_factor = 1
+            result = []
+            while max_factor - min_factor > 0.01:
+                factor = (min_factor + max_factor) / 2
+                curve = Bezier(
+                      best_curve.p0,
+                      best_curve.p0 + (best_curve.p1 - best_curve.p0) * factor,
+                      best_curve.p3 + (best_curve.p2 - best_curve.p3) * factor,
+                      best_curve.p3)
+                filled = apply_new_curve(curve)
+                if abs(len(filled) - integral) <= integral_tolerance:
+                    result = filled
+                    break
+                if len(filled) > integral + integral_tolerance:
+                    max_factor = factor
+                else:
+                    min_factor = factor
+            if not result:
+                print("Binary search failed")
+                continue
+            
+            for x in range(seed.x - size + 1, seed.x + size):
+                for y in range(seed.y - size + 1, seed.y + size):
+                    if Point(x, y) in result:
+                        self.pixels[x, y] = 50
+                    else:
+                        self.pixels[x, y] = 0
 
     def center(self):
         deltax = (self.h - self.x_min() - self.x_max() - 1) // 2
@@ -218,7 +425,7 @@ def performe_folding(input_img, num_examples, max_fold_count, min_fold_area, xy_
 
 def main():
 
-    folded_imgs=performe_folding("input/unfolded.png",100)
+    #folded_imgs=performe_folding("input/unfolded.png", 100)
 
     seed = Bild(Image.open("input/unfolded.png")
                      .resize((200, 200), resample=Image.NEAREST)
@@ -230,13 +437,13 @@ def main():
 
     data = [seed]
 
-    for i in range(100):
+    for i in range(10):
         while True:
             seed = random.choice(data)
             if seed.foldcount < 4:
                 break
         image = seed.copy()
-        image.fold(0.2)
+        image.fold(0.2, xy_axis_bias=True)
         image.center()
         data.append(image)
 
